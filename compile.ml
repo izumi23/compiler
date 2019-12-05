6fb94ae48635c61b128aa3d792da422d86d86109
izumi23 <izumiwatanabe23@gmail.com>
2019-12-05 13:14:42 +0100

    Déclarations de chaînes de caractères

--- compile.ml
+++ compile.ml
@@ -18,79 +18,129 @@ let compile out decl_list =
   let p = Printf.fprintf in
   let var_count = ref 0 in
   let var = ref (Hashtbl.create 8) in
-  let global_var = ref true in
-  let var_location = ref "" in
 
 
 
-  let rec compile_decl_list = function
+(*Renvoie l'adresse de la variable s, selon qu'elle soit locale ou globale*)
+
+  let var_location s =
+    if Hashtbl.mem !var s then
+      let i = Hashtbl.find !var s in
+      Printf.sprintf "%d(%%rbp)" (-8*i)
+    else Printf.sprintf "%s(%%rip)" s
+  in
+
+
+
+(*Fonctions pour une première lecture, pour stocker les chaines de caracteres*)
+
+  let strings = Hashtbl.create 8 in
+  let string_count = ref 0 in
+
+  let rec first_decl_list lcl = List.iter first_decl lcl
+
+  and first_decl = function
+  | CDECL _ -> ()
+  | CFUN (_, _, _, lc) -> first_code lc
+
+  and first_code (_, c) = match c with
+  | CBLOCK (vdl, lcl) -> List.iter first_code lcl
+  | CEXPR e -> first_expr e
+  | CIF (e, c1, c2) ->
+      first_expr e; first_code c1; first_code c2
+  | CWHILE (e, c) -> first_expr e; first_code c
+  | CRETURN None -> ()
+  | CRETURN (Some e) -> first_expr e
+
+  and first_expr (_, e) = match e with
+
+  (*le seul cas important!*)
+  | STRING s when not (Hashtbl.mem strings s) ->
+      p out "LC%d:\n        .string %S\n" !string_count s;
+      Hashtbl.add strings s !string_count;
+      incr string_count
+
+  | CALL (_, lel) -> List.iter first_expr lel
+  | EIF (_, e1, e2) -> first_expr e1; first_expr e2
+  | ESEQ lel -> List.iter first_expr lel
+  | _ -> ()
+
+  in
+
+
+
+
+
+(*Fonctions principales qui compilent le code*)
+
+
+  let rec compile_decl_list globl = function
   | [] -> ()
-  | h::t -> compile_decl h; compile_decl_list t
+  | h::t -> compile_decl globl h; compile_decl_list globl t
 
 
 
-  and compile_mop = function
+  and compile_mop mop (l, e) = match mop, e with
 
-  | M_MINUS -> p out "        negq    %%rax\n"
-  | M_NOT -> p out "        notq    %%rax\n"
-  | M_POST_INC -> p out "        addq    $1, %s\n" !var_location
-  | M_POST_DEC -> p out "        subq    $1, %s\n" !var_location
+  | M_MINUS, _ ->
+      compile_expr (l, e);
+      p out "        negq    %%rax\n"
 
-  | M_PRE_INC ->
-      p out "        addq    $1, %s\n" !var_location;
+  | M_NOT, _ ->
+      compile_expr (l, e);
+      p out "        notq    %%rax\n"
+
+  | M_POST_INC, VAR s ->
+      p out "        addq    $1, %s\n" (var_location s)
+
+  | M_POST_DEC, VAR s ->
+      p out "        subq    $1, %s\n" (var_location s)
+
+  | M_PRE_INC, VAR s ->
+      p out "        addq    $1, %s\n" (var_location s);
       p out "        addq    $1, %%rax\n"
 
-  | M_PRE_DEC ->
-      p out "        subq    $1, %s\n" !var_location;
+  | M_PRE_DEC, VAR s ->
+      p out "        subq    $1, %s\n" (var_location s);
       p out "        subq    $1, %%rax\n"
 
+  | _ -> failwith "cas impossible"
 
 
-  and compile_expr = function
+
+  and compile_expr (_, e) = match e with
 
   | VAR s ->
-      if Hashtbl.mem !var s then begin
-        let i = Hashtbl.find !var s in
-        var_location := Printf.sprintf "%d(%%rbp)" (-8*i);
-        p out "        movq    %s, %%rax\n" !var_location
-      end
-      else todo "Variable globale"
+      p out "        movq    %s, %%rax\n" (var_location s)
 
   | CST n ->
       p out "        movq    $%d, %%rax\n" n;
 
-  | SET_VAR (s, (_, e)) ->
+  | SET_VAR (s, e) ->
       compile_expr e;
-      if Hashtbl.mem !var s then begin
-        let i = Hashtbl.find !var s in
-        var_location := Printf.sprintf "%d(%%rbp)" (-8*i);
-        p out "        movq    %%rax, %s\n" !var_location
-      end
-      else todo "Variable globale"
+      p out "        movq    %%rax, %s\n" (var_location s)
 
-  | OP1 (mop, (_, e)) ->
-      compile_expr e;
-      compile_mop mop;
+  | OP1 (mop, e) ->
+      compile_mop mop e;
 
   | _ -> todo "a completer"
 
 
 
 
-  and compile_decl = function
+  and compile_decl globl = function
 
   | CDECL (_, s) ->
-      incr var_count;
-      Hashtbl.add !var s !var_count;
-      if !global_var then todo "Cas d'une variable globale"
+      if globl then p out "        .comm   %s,8,8\n" s
+      else (incr var_count; Hashtbl.add !var s !var_count)
 
   | CFUN (_, s, vdl, lc) ->
-      global_var := false;
       var_count := 0;
       var := Hashtbl.create 8;
-      p out "        .globl  %s\n        .type   %s, @function\n%s:\n" s s s;
+      p out "        .globl  %s\n" s;
+      p out "        .type   %s, @function\n%s:\n" s s;
       p out "        pushq   %%rbp\n        movq    %%rsp, %%rbp\n";
-      List.iter compile_decl vdl;
+      compile_decl_list false vdl;
       compile_code lc
 
 
@@ -99,26 +149,31 @@ let compile out decl_list =
   and compile_code (_,c) = match c with
 
   | CBLOCK (vdl, lcl) ->
-      (*a completer*)
-      List.iter compile_decl vdl;
+      compile_decl_list false vdl;
+      if !var_count > 0 then
+        p out "        subq    $%d, %%rsp\n" (8* !var_count);
       List.iter compile_code lcl
 
   | CRETURN leo ->
       begin
       match leo with
       | None -> ()
-      | Some (_, e) -> compile_expr e
+      | Some e -> compile_expr e
       end;
       p out "        leave\n        ret\n"
 
-  | CEXPR (_, e) -> compile_expr e
+  | CEXPR e -> compile_expr e
 
 
   | _ -> todo ("not cblock")
 
 
+
+
   in
+  p out "        .text\n        .section        .rodata\n";
+  first_decl_list decl_list;
   p out "        .text\n";
-  compile_decl_list decl_list
+  compile_decl_list true decl_list
 
 

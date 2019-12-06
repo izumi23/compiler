d8d8a54e63544ee7ae04f7336b7c72bc017b7925
izumi23 <izumiwatanabe23@gmail.com>
2019-12-06 19:26:16 +0100

    Optimisation des expressions Et correction de l'allocation sur la pile

--- compile.ml
+++ compile.ml
@@ -16,10 +16,19 @@ let compile out decl_list =
 
   let todo s = Printf.fprintf out "#TODO %s\n" s in
   let p = Printf.fprintf in
+
+
+
+(*var associe à chaque variable locale sa position relative sur la pile.
+  Pour cela on maintient à jour le compteur var_count.*)
   let var_count = ref 0 in
   let var = ref (Hashtbl.create 8) in
+
+(*Une liste des noms de fonctions*)
   let functions = ref [] in
+
   let arg_registers = [|"%rdi"; "%rsi"; "%rdx"; "%rcx"; "%r8"; "%r9"|] in
+
 
 
 
@@ -35,7 +44,7 @@ let compile out decl_list =
 
 
 
-(*Fonctions pour une première lecture, pour stocker les chaines de caracteres*)
+(*Fonctions pour une première lecture, pour stocker les chaînes de caractères*)
 
   let strings = Hashtbl.create 8 in
   let string_count = ref 0 in
@@ -47,13 +56,11 @@ let compile out decl_list =
   | CFUN (_, _, _, lc) -> first_code lc
 
   and first_code (_, c) = match c with
-  | CBLOCK (vdl, lcl) -> List.iter first_code lcl
+  | CBLOCK (_, lcl) -> List.iter first_code lcl
   | CEXPR e -> first_expr e
-  | CIF (e, c1, c2) ->
-      first_expr e; first_code c1; first_code c2
-  | CWHILE (e, c) -> first_expr e; first_code c
-  | CRETURN None -> ()
-  | CRETURN (Some e) -> first_expr e
+  | CIF (_, c1, c2) -> first_code c1; first_code c2
+  | CWHILE (_, c) -> first_code c
+  | _ -> ()
 
   and first_expr (_, e) = match e with
 
@@ -88,11 +95,11 @@ let compile out decl_list =
   and compile_mop mop (l, e) = match mop, e with
 
   | M_MINUS, _ ->
-      compile_expr (l, e);
+      compile_expr "%rax" (l, e);
       p out "        negq    %%rax\n"
 
   | M_NOT, _ ->
-      compile_expr (l, e);
+      compile_expr "%rax" (l, e);
       p out "        notq    %%rax\n"
 
   | M_POST_INC, VAR s ->
@@ -113,35 +120,49 @@ let compile out decl_list =
 
 
 
-  and push_arg i = function
+
+(*Appelé lors d'un CALL, compile les arguments donnés à la fonction appelée.
+  Pour cela on conserve dans i le rang du prochain argument à traiter.*)
+
+  and push_args i = function
   | [] -> ()
   | e::t ->
-      compile_expr e;
-      if i < 6 then p out "        movq    %%rax, %s\n" arg_registers.(i)
-      else p out "        pushq   %%rax\n";
-      push_arg (i-1) t
+      if i < 6 then compile_expr arg_registers.(i) e
+      else compile_expr "STACK" e;
+      push_args (i-1) t
 
 
 
-  and compile_expr (_, e) = match e with
+
+(*Quelques optimisations pour les cas VAR, CST et STRING, où l'on écrit
+  directement sur un registre donné ou la pile, selon la valeur de dest.*)
+
+  and compile_expr dest (_, e) = match e with
 
   | VAR s ->
-      p out "        movq    %s, %%rax\n" (var_location s)
+      if dest = "STACK" then p out "        pushq   %s\n" (var_location s)
+      else p out "        movq    %s, %s\n" (var_location s) dest
 
   | CST n ->
-      p out "        movq    $%d, %%rax\n" n;
+      if dest = "STACK" then p out "        pushq   $%d\n" n
+      else p out "        movq    $%d, %s\n" n dest
 
   | STRING s ->
-      p out "        leaq    .LC%d(%%rip), %%rax\n" (Hashtbl.find strings s)
+      let loc = Hashtbl.find strings s in
+      if dest = "STACK" then begin
+        p out "        leaq    .LC%d(%%rip), %%rax\n" loc;
+        p out "        pushq   %%rax\n"
+      end
+      else p out "        leaq    .LC%d(%%rip), %s\n" loc dest
 
   | SET_VAR (s, e) ->
-      compile_expr e;
+      compile_expr "%rax" e;
       p out "        movq    %%rax, %s\n" (var_location s)
 
   | CALL (s, lel) ->
       if List.length lel > 6 && List.length lel mod 2 = 1 then
         p out "        subq    $8, %%rsp\n";
-      push_arg (List.length lel - 1) (List.rev lel);
+      push_args (List.length lel - 1) (List.rev lel);
       if List.mem s !functions then p out "        call    %s\n" s
       else p out "        call    %s@PLT\n"s
 
@@ -175,20 +196,23 @@ let compile out decl_list =
   and compile_code (_,c) = match c with
 
   | CBLOCK (vdl, lcl) ->
+      let n = List.length vdl in
+      if n > 0 then
+        let offset = 8*(n + (n mod 2)) in
+        p out "        subq    $%d, %%rsp\n" offset;
+      if n mod 2 = 1 then incr var_count;
       compile_decl_list false vdl;
-      if !var_count > 0 then
-        p out "        subq    $%d, %%rsp\n" (8*(!var_count+(!var_count mod 2)));
       List.iter compile_code lcl
 
   | CRETURN leo ->
       begin
       match leo with
       | None -> ()
-      | Some e -> compile_expr e
+      | Some e -> compile_expr "%rax" e
       end;
       p out "        leave\n        ret\n"
 
-  | CEXPR e -> compile_expr e
+  | CEXPR e -> compile_expr "%rax" e
 
 
   | _ -> todo ("not cblock")
